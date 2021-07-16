deb42d41cb32656e6de5bc61fd9a43a5824610ff
seonb2n <626ksb@gmail.com>
2021-07-16 11:28:32 +0900

    pjt create

---
+++ src/main/java/com/example/demo/LogInAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogInAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogInAppApplication.class, args);
+	}
+
+}
